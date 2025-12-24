b0eaf324c071a6a5793841882f76add1bcfab66b
Sviatoslav.Rovenchak <djedaimo@gmail.com>
2025-12-24 16:46:53 +0200

    Prevent self-kick from the admin panel

--- simple-cli-messenger/crates/server/src/server/client/reader.rs
+++ simple-cli-messenger/crates/server/src/server/client/reader.rs
@@ -64,14 +64,16 @@ impl ClientReader {
                     // remove user object
                     // next arc clone is held inside client specific task
                     // its connection will be dropped as soon as it receives kick message
-                    self.server_state
-                        .connected_clients
-                        .write()
-                        .await
-                        .remove(&kicked_username);
-                    self.tx_to_message_distributor
-                        .send(ClientMessage::Kick(common, kicked_username))
-                        .await?
+                    if common.username != kicked_username {
+                        self.server_state
+                            .connected_clients
+                            .write()
+                            .await
+                            .remove(&kicked_username);
+                        self.tx_to_message_distributor
+                            .send(ClientMessage::Kick(common, kicked_username))
+                            .await?
+                    }
                 }
                 _ => {
                     tracing::info!("Received message from client {}: {:?}", self.username, msg);
